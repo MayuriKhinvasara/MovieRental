0ebe7e38c33f355ab6bd3424a74a20195446d9aa
Ashwini Mutalik Desai <ashwinidesai2310@gmail.com>
2016-05-11 15:42:58 +0530

    Arathy/Nikitha/Ashwini - Adding a demo junit test

---
+++ src/main/test/com/thoughtworks/movierental/CustomerTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.movierental;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void testJunit() {
+        assertEquals(true, true);
+    }
+
+}
